e48a08f15d6ab51b052495f2689cf60ca8a74634
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-07 18:26:38 -0400

    Updates

---
+++ precache-manifest.6bcbc975d8bd21eaa9c6ef743d744961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4ad66874963227f991f95363f00929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6753e0a36b419440ec3b",
+    "url": "/static/css/main.7e8fbc56.chunk.css"
+  },
+  {
+    "revision": "50fccf01c02a0474b9cd",
+    "url": "/static/js/2.20402dd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20402dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6753e0a36b419440ec3b",
+    "url": "/static/js/main.91fca71c.chunk.js"
+  },
+  {
+    "revision": "29eb28e32d5429d10d76",
+    "url": "/static/js/runtime-main.fc2068dd.js"
+  }
+]);
