279cb7accec1e2e1ea4a0e82499f96327b8494f1
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-07 16:09:39 -0400

    Adding dropdown menu

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import TableRow from "./TableRow";
+
 export class Table extends Component {
-    constructor() {
-      super();
-      this.state = {
-        rows: 0,
-        cells: 1,
-      };
-    }
-    render() {
-      return (
-        <>
-          <div id="buttons">
-            <button>Add a row</button>
-            <button>Add cells</button>
-            <button>Remove a row</button>
-            <button >Remove a cell</button>
-          </div>
-        </>
-      );
-    }
+  constructor() {
+    super();
+    this.state = {
+      rows: 0,
+      cells: 1,
+      defaultColor: "blue",
+      selectedColor: null,
+    };
   }
-  
-  export default Table;
+
+  render() {
+    return (
+      <>
+        <div id="buttons">
+          <button onClick={this.addNewRow}>Add a row</button>
+          <button onClick={this.addNewCol}>Add a Column</button>
+          <button onClick={this.removeRow}>Remove a row</button>
+          <button onClick={this.removeCol}>Remove a Column</button>
+        </div>
+        <span>Select a color:</span>
+        <select onChange={this.changeColor}>
+          <option value={this.state.defaultColor}>Please select a Color</option>
+          <option value="red">Blue</option>          
+          <option value="green">Green</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+        </select>
+      </>
+    );
+  }
+}
+
+export default Table;
