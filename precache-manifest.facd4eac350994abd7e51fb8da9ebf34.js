72bd0c3e3384c42ed10a9d1c66341393e9613fd5
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-08 10:32:05 -0400

    Updates

---
+++ precache-manifest.facd4eac350994abd7e51fb8da9ebf34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d677bdcb6560915f6050273aab1ca9f",
+    "url": "/Grid/index.html"
+  },
+  {
+    "revision": "bff1387f6de8fe177fef",
+    "url": "/Grid/static/css/main.7e8fbc56.chunk.css"
+  },
+  {
+    "revision": "50fccf01c02a0474b9cd",
+    "url": "/Grid/static/js/2.20402dd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grid/static/js/2.20402dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff1387f6de8fe177fef",
+    "url": "/Grid/static/js/main.de0f07b2.chunk.js"
+  },
+  {
+    "revision": "065a44dc5355100837cb",
+    "url": "/Grid/static/js/runtime-main.80fbbec6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bcbc975d8bd21eaa9c6ef743d744961.js"
+  "/Grid/precache-manifest.facd4eac350994abd7e51fb8da9ebf34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Grid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
